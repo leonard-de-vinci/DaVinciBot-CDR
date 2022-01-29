fc0cdce8fcca621e166108ee21737ec839eda025
Thomas LEDOS <thomas.ledos92@gmail.com>
2022-01-29 15:49:22 +0100

    py: updated setup requirements

--- python_packages/setup.py
+++ python_packages/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(),
     name="dvbcdr",
-    version="0.0.1-alpha",
-    install_requires=["numpy", "pyserial", "pyyaml", "pygame", "pyaccesspoint"]
+    version="0.0.2",
+    install_requires=["numpy", "pyserial", "pyyaml", "pygame", "pyaccesspoint", "pysicktim", "pyusb", "sklearn"]
 )
