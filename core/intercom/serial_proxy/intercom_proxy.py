1483ecd261546e0052817cf905e1d9e3dd340757
Thomas LEDOS <thomas.ledos92@gmail.com>
2022-02-19 14:41:34 +0100

    Corrected bugs and added retries to intercom proxy

--- core/intercom/serial_proxy/intercom_proxy.py
+++ core/intercom/serial_proxy/intercom_proxy.py
@@ -11,15 +11,25 @@ import json
 
 config = {}
 opened_ports = dvbcdr.utils.thread_safe.ThreadSafeList()
+retries = dvbcdr.utils.thread_safe.ThreadSafeDict()
 
 
 def check_ports():
-    ports = serial.tools.list_ports.comports()
-    with opened_ports:
-        for port in ports:
-            if(port.device not in opened_ports):
-                port_thread = threading.Thread(target=handle_port, args=(port.device,))
-                port_thread.start()
+    ports = [x.device for x in serial.tools.list_ports.comports()]
+    with opened_ports, retries:
+        for port_path in ports:
+            if(port_path not in opened_ports):
+                if port_path not in retries:
+                    retries[port_path] = 0
+
+                if retries[port_path] < 3:
+                    retries[port_path] += 1
+                    threading.Thread(target=handle_port, args=(port_path, retries[port_path]), daemon=True).start()
+
+        already_tried_ports = list(retries.keys())
+        for old_port in already_tried_ports:
+            if old_port not in ports:
+                del retries[old_port]
 
 
 def __get_data_type(data):
@@ -33,8 +43,12 @@ def __get_data_type(data):
     return (-1, None)
 
 
-def __data_received(port_serial, data):
-    data_type, = __get_data_type(data)
+def __get_data_received(crc_subject, port_serial):
+    return lambda r: __data_received(port_serial, crc_subject, r)
+
+
+def __data_received(port_serial, crc_subject, data):
+    data_type, _ = __get_data_type(data)
     length = -1
     if data_type == 1:
         data = "\"" + data.replace("\"", "\\\"") + "\""
@@ -49,21 +63,24 @@ def __data_received(port_serial, data):
             data = "[" + ",".join([("\"" + x + "\"" if data_type == 1 else str(x)) for x in data if type(x) == type_ctor]) + "]"
 
     if data_type in [0, 1, 2]:
-        port_serial.write(bytes("{\"c\":2,\"t\":" + str(data_type) + (",\"l\":" + str(length) if length >= 0 else "") + ",\"d\":" + str(data) + "}\n", "utf-8"))
+        port_serial.write(bytes("{\"c\":2,\"t\":" + str(data_type) + (",\"l\":" + str(length) if length >= 0 else "") + ",\"s\":" + str(crc_subject) + ",\"v\":" + str(data) + "}\n", "utf-8"))
 
 
 def __event_received(port_serial, event):
     port_serial.write(bytes("{\"c\":3,\"e\":\"" + event + "\"}\n", "utf-8"))
 
 
-def handle_port(port_path):
+def handle_port(port_path, retry_count):
     with opened_ports:
         opened_ports.append(port_path)
-    print("Opening", port_path + "...")
+
+    print("Opening", port_path, "(" + str(retry_count) + "/3)...")
+    opening_time = time.time()
+    device_id = None
 
     try:
         baudrate = config.get("baudrate", 115200)
-        port_serial = serial.Serial(port_path, baudrate=baudrate)
+        port_serial = serial.Serial(port_path, baudrate=baudrate, timeout=0)
         port_serial.write(b"{\"c\":0}")
 
         # creating an Intercom instance
@@ -72,7 +89,16 @@ def handle_port(port_path):
         intercom.wait_in_new_thread()
 
         while True:
+            if device_id is None and time.time() - opening_time > (2 + retry_count):
+                print(port_path, "-> connection timeout!")
+                port_serial.close()
+                time.sleep(retry_count * 2)
+                break
+
             raw_data = port_serial.readline()
+            if len(raw_data) == 0:
+                continue
+
             try:
                 data = json.loads(raw_data)
             except JSONDecodeError:
@@ -84,8 +110,13 @@ def handle_port(port_path):
 
                 # TODO: maybe we can use python 3.10 match in the future
                 if command == 0:
-                    # TODO: save the device id and kill this thread if timeout
-                    port_serial.write(b"{\"c\":1}")
+                    if device_id is None:
+                        device_id = "unknown"
+                        if "v" in data:
+                            device_id = data["v"]
+
+                        print(port_path, "->", device_id, "connected!")
+                        port_serial.write(b"{\"c\":1}")
                 elif command == 1:
                     port_serial.write(b"{\"c\":1}")
                 elif command == 2:
@@ -99,7 +130,7 @@ def handle_port(port_path):
                         if data_type in [0, 1, 2]:
                             intercom.publish_raw(crc_subject, data)
                         elif data_type == 255:
-                            intercom.subscribe_raw(crc_subject, lambda r: __data_received(port_serial, r))
+                            intercom.subscribe_raw(crc_subject, __get_data_received(crc_subject, port_serial))
                             port_serial.write(b"{\"c\":1}")
                 elif command == 3:
                     intercom.publish_event(data["e"])
