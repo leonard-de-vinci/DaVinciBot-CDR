70d35a83d37b846c0f200c3d584f0bcfd0b775b3
Thomas LEDOS <thomas.ledos92@gmail.com>
2022-02-25 13:36:57 +0100

    Added base of display cabinet code

--- firmware/display_cabinet/include/constants.h
+++ firmware/display_cabinet/include/constants.h
@@ -1,1 +1,4 @@
-#define PIN_SENSOR A7
+#define PIN_SENSOR 7
+#define PIN_LEDSTRIP 4
+#define PIN_SERVO 5
+#define NUM_LEDS 38
--- firmware/display_cabinet/src/main.cpp
+++ firmware/display_cabinet/src/main.cpp
@@ -1,11 +1,50 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <PWMServo.h>
 #include "constants.h"
 
-void setup() {
+bool detected = true;
+CRGB leds[NUM_LEDS];
 
+PWMServo dinoServo;
+
+void setup() {
+    pinMode(PIN_SENSOR, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
+    
+    dinoServo.attach(PIN_SERVO);
+    FastLED.addLeds<NEOPIXEL, 4>(leds, NUM_LEDS);
 }
 
-void loop() {
+int state = 0;
+bool servoState = false;
 
+void loop() {
+    if(digitalRead(PIN_SENSOR) == LOW) {
+        detected = true;
+    }
+
+    if(detected) {
+        delay(300);
+        for(int i = 0; i < NUM_LEDS; i++) {
+            int pos = (i + state) % NUM_LEDS;
+            leds[pos] = (i/2) % 2 == 0 ? CRGB::Purple : CRGB::Yellow;
+        }
+
+        FastLED.show();
+
+        if(state == 0) {
+            if(servoState) {
+                dinoServo.write(0);
+                digitalWrite(LED_BUILTIN, LOW);
+            } else {
+                dinoServo.write(180);
+                digitalWrite(LED_BUILTIN, HIGH);
+            }
+
+            servoState = !servoState;
+        }
+
+        state = (state + 1) % 4;
+    }
 }
